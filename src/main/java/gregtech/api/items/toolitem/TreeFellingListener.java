986b4825ab1080c9805a4181ce5de90c71ab341e
PrototypeTrousers <salomaoo@gmail.com>
2023-01-10 12:43:20 -0300

    wip tick spreading

--- src/main/java/gregtech/api/items/toolitem/TreeFellingListener.java
+++ src/main/java/gregtech/api/items/toolitem/TreeFellingListener.java
@@ -1,6 +1,6 @@
 package gregtech.api.items.toolitem;
 
-import it.unimi.dsi.fastutil.objects.ObjectOpenHashSet;
+import gregtech.api.util.GTLog;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
@@ -16,8 +16,9 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 import javax.annotation.Nonnull;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
 
 import static gregtech.api.items.toolitem.ToolHelper.RELOCATE_MINED_BLOCKS_KEY;
 
@@ -25,62 +26,35 @@ public final class TreeFellingListener {
 
     private final EntityPlayerMP player;
     private final ItemStack tool;
-    private final Deque<BlockPos> orderedBlocks;
-    private Deque<BlockPos> leafBlocks;
 
-    private TreeFellingListener(EntityPlayerMP player, ItemStack tool, Deque<BlockPos> orderedBlocks) {
+    private ArrayDeque<BlockPos> futureLayer;
+    private Deque<BlockPos> orderedBlocks;
+    private final Deque<BlockPos> leafBlocks;
+    private final Block logBlock;
+    private final BlockPos startPos;
+    private int pass = 0;
+
+    private TreeFellingListener(EntityPlayerMP player, ItemStack tool, IBlockState state, BlockPos start) {
         this.player = player;
         this.tool = tool;
-        this.orderedBlocks = orderedBlocks;
+        this.logBlock = state.getBlock();
+        this.startPos = start;
+        this.orderedBlocks = new ArrayDeque<>();
+        this.futureLayer = new ArrayDeque<>();
+        this.leafBlocks = new ArrayDeque<>();
     }
 
     public static void start(@Nonnull IBlockState state, ItemStack tool, BlockPos start, @Nonnull EntityPlayerMP player) {
-        World world = player.world;
-        Block block = state.getBlock();
-        BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
-
-        Queue<BlockPos> checking = new ArrayDeque<>();
-        Set<BlockPos> visited = new ObjectOpenHashSet<>();
-        checking.add(start);
-
-        while (!checking.isEmpty()) {
-            BlockPos check = checking.remove();
-            if (check != start) {
-                visited.add(check);
-            }
-            for (int y = 0; y <= 1; y++) {
-                for (int x = -1; x <= 1; x++) {
-                    for (int z = -1; z <= 1; z++) {
-                        if (x != 0 || y != 0 || z != 0) {
-                            mutablePos.setPos(check.getX() + x, check.getY() + y, check.getZ() + z);
-                            if (!checking.contains(mutablePos)) {
-                                if (!visited.contains(mutablePos)) {
-                                    // Check that the found block matches the original block state, which is wood.
-                                    if (block == world.getBlockState(mutablePos).getBlock()) {
-                                        BlockPos immutablePos = mutablePos.toImmutable();
-                                        checking.add(immutablePos);
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!visited.isEmpty()) {
-            Deque<BlockPos> orderedBlocks = visited.stream()
-                    .sorted(Comparator.comparingInt(pos -> start.getY() - pos.getY()))
-                    .collect(Collectors.toCollection(ArrayDeque::new));
-            MinecraftForge.EVENT_BUS.register(new TreeFellingListener(player, tool, orderedBlocks));
-        }
+        MinecraftForge.EVENT_BUS.register(new TreeFellingListener(player, tool, state, start));
     }
 
     @SubscribeEvent
     public void onWorldTick(@Nonnull TickEvent.WorldTickEvent event) {
         if (event.phase == TickEvent.Phase.START && event.world == player.world && event.side == Side.SERVER) {
-            if (leafBlocks == null) {
+            if (orderedBlocks.isEmpty()) {
+                gatherLogLayer();
                 gatherLeafs(event.world);
+                pass++;
             }
 
             if (orderedBlocks.isEmpty()) {
@@ -103,6 +77,7 @@ public final class TreeFellingListener {
                     player.world.setBlockToAir(check);
                 }
                 MinecraftForge.EVENT_BUS.unregister(this);
+                GTLog.logger.info("Total gathering ticks: " + pass);
                 return;
             }
 
@@ -114,13 +89,58 @@ public final class TreeFellingListener {
         }
     }
 
+    private void gatherLogLayer() {
+        World world = player.world;
+        BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
+
+        if (pass == 0) {
+            futureLayer.add(startPos);
+        }
+
+        ArrayDeque<BlockPos> currentLayer = futureLayer;
+        this.futureLayer = new ArrayDeque<>();
+
+        ArrayDeque<BlockPos> visited = new ArrayDeque<>();
+        ArrayDeque<BlockPos> toBreak = new ArrayDeque<>();
+
+        while (!currentLayer.isEmpty()) {
+            BlockPos check = currentLayer.remove();
+            toBreak.add(check);
+
+            for (int y = 0; y <= 1; y++) {
+                for (int x = -1; x <= 1; x++) {
+                    for (int z = -1; z <= 1; z++) {
+                        if (x != 0 || y != 0 || z != 0) {
+                            mutablePos.setPos(check.getX() + x, check.getY() + y, check.getZ() + z);
+                            if (!visited.contains(mutablePos)) {
+                                BlockPos immutablePos = mutablePos.toImmutable();
+                                if (!currentLayer.contains(mutablePos)) {
+                                    // Check that the found block matches the original block state, which is wood.
+                                    if (logBlock == world.getBlockState(mutablePos).getBlock()) {
+                                        if (y == 0) {
+                                            currentLayer.add(immutablePos);
+                                        } else {
+                                            futureLayer.add(immutablePos);
+                                        }
+                                    }
+                                }
+                                visited.add(immutablePos);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        this.orderedBlocks = toBreak;
+    }
+
     private void gatherLeafs(World world) {
         //Gather the leaves from an area determined by the amount of faces a log block not touching another log block.
         BlockPos.MutableBlockPos mutablePos = new BlockPos.MutableBlockPos();
         BlockPos.MutableBlockPos maxXYZ = new BlockPos.MutableBlockPos();
         BlockPos.MutableBlockPos minXYZ = new BlockPos.MutableBlockPos();
-        Set<BlockPos> tempLB = new ObjectOpenHashSet<>();
-        leafBlocks = new ArrayDeque<>();
+        ArrayDeque<BlockPos> toBreak = new ArrayDeque<>();
+        ArrayDeque<BlockPos> visited = new ArrayDeque<>();
 
         int faces = 0;
 
@@ -130,48 +150,60 @@ public final class TreeFellingListener {
             for (EnumFacing e : EnumFacing.VALUES) {
                 mutablePos.setPos(b);
                 mutablePos.move(e);
+                if (pass == 1) {
+                    if (mutablePos.equals(startPos)) {
+                        continue;
+                    }
+                }
                 IBlockState bs = world.getBlockState(mutablePos);
                 if (bs.getMaterial() == Material.LEAVES || bs.getBlock() == Blocks.AIR) {
-                    if (bs.getMaterial() == Material.LEAVES) {
-                        //add this neighbor to save 1 getBlockState call later
-                        tempLB.add(mutablePos.toImmutable());
-                    }
                     //Add the face to be checked later
                     faces = faces | 1 << e.getIndex();
                 }
             }
             if (faces > 0) {
-                minXYZ.setPos(b);
-                maxXYZ.setPos(b);
-                for (EnumFacing f : EnumFacing.VALUES) {
-                    //if the face was added grow in that direction
-                    if ((faces & 1 << f.getIndex()) > 0) {
-                        if (f.getAxisDirection() == EnumFacing.AxisDirection.POSITIVE) {
-                            maxXYZ.move(f, 3);
-                        } else {
-                            minXYZ.move(f, 3);
+                for (int distance = 1; distance < 4; distance++) {
+                    minXYZ.setPos(b);
+                    maxXYZ.setPos(b);
+                    boolean anyLeafs = false;
+                    for (EnumFacing facing : EnumFacing.VALUES) {
+                        //if the face was added grow in that direction
+                        if ((faces & 1 << facing.getIndex()) > 0) {
+                            if (facing.getAxisDirection() == EnumFacing.AxisDirection.POSITIVE) {
+                                maxXYZ.move(facing, distance);
+                            } else {
+                                minXYZ.move(facing, distance);
+                            }
                         }
                     }
-                }
-                //checks the blocks in the volume determined before for leaf blocks
-                for (int y = minXYZ.getY(); y <= maxXYZ.getY(); ++y) {
-                    for (int x = minXYZ.getX(); x <= maxXYZ.getX(); ++x) {
-                        for (int z = minXYZ.getZ(); z <= maxXYZ.getZ(); ++z) {
-                            if (x != 0 || y != 0 || z != 0) {
-                                mutablePos.setPos(x, y, z);
-                                if (!tempLB.contains(mutablePos)) {
-                                    IBlockState bs = world.getBlockState(mutablePos);
-                                    if (bs.getMaterial() == Material.LEAVES) {
-                                        tempLB.add(mutablePos.toImmutable());
+                    //checks the blocks in the volume determined before for leaf blocks
+                    for (int y = minXYZ.getY(); y <= maxXYZ.getY(); y++) {
+                        for (int x = minXYZ.getX(); x <= maxXYZ.getX(); x++) {
+                            for (int z = minXYZ.getZ(); z <= maxXYZ.getZ(); z++) {
+                                if (x != 0 || y != 0 || z != 0) {
+                                    mutablePos.setPos(x, y, z);
+                                    if (!visited.contains(mutablePos)) {
+                                        BlockPos immutable = mutablePos.toImmutable();
+                                        if (!toBreak.contains(mutablePos)) {
+                                            IBlockState bs = world.getBlockState(mutablePos);
+                                            if (bs.getMaterial() == Material.LEAVES) {
+                                                toBreak.add(immutable);
+                                                anyLeafs = true;
+                                            }
+                                        }
+                                        visited.add(immutable);
                                     }
                                 }
                             }
                         }
                     }
+                    if (!anyLeafs) {
+                        break;
+                    }
                 }
             }
             faces = 0;
         }
-        leafBlocks.addAll(tempLB);
+        leafBlocks.addAll(toBreak);
     }
 }
